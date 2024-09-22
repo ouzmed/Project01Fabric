907b0d78eade36b8286e79f5fdb20977adffd244
ouzmed <148863961+ouzmed@users.noreply.github.com>
2024-09-22 19:10:27 +0200

    Committing 1 item from workspace 1237831a-c12b-499a-9dcd-7f59f765fda8

--- bing_warehouse_db.Warehouse/dbo/Tables/provider_agg.sql
+++ bing_warehouse_db.Warehouse/dbo/Tables/provider_agg.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[provider_agg] (
 
-	[provider] varchar(60) NULL, 
-	[sentiment] varchar(10) NULL, 
-	[nombre] int NULL
+	[provider] varchar(255) NULL, 
+	[sentiment] varchar(255) NULL, 
+	[Total] int NULL
 );
 
